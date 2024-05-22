7f6b0f7af396683f1465d79fa3ab6284367dc592
Agustin Gomez del toro <agustingomezdeltoro@MacBook-Pro-de-Agustin.local>
2024-05-22 17:58:16 +0200

    privacy gomez del toro

--- script/Privacy.sol
+++ script/Privacy.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.6.0;
+pragma solidity ^0.8.0;
 
 import "forge-std/Script.sol";
-import "../instances/instancePrivacy.sol";
+import "../instances/intancePrivacy.sol";
 
 contract POC is Script {
 
